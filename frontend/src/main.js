c7eb3d93daa9ec7aa2624d593da7272ee666365f
André Théo LAURET <andrelauret@icloud.com>
2023-01-29 10:14:08 +0400

    Move frontend to specific folder

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import './main.css'
 
 createApp(App).mount('#app')
