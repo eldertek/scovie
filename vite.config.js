b49d19dfe496d57d83cd0da1802308f1d5caa3a1
André Théo LAURET <andrelauret@icloud.com>
2023-01-29 09:53:23 +0400

    Change base path to relative

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { build } from 'esbuild'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0'
-  }
+  },
+  build: {
+    cssCodeSplit: false,
+  },
+  base: './',
 })
