bb863d6f691264b448604f6a8eb786cb823e7ccc
André Théo LAURET <andrelauret@icloud.com>
2023-01-29 09:08:22 +0400

    Initial commit

---
+++ src/components/screen/FooterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav :class="{'fixed-bottom': isDesktop, 'bottom': !isDesktop}" class="navbar bg-white" style="--bs-bg-opacity: .20;">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1 fs-6 text-start text-light">
+                Développé par André Théo LAURET
+            </span>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    name: "FooterComponent",
+    data() {
+        return {
+            isDesktop: window.innerWidth > 767
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.handleResize)
+    },
+    methods: {
+        handleResize() {
+            this.isDesktop = window.innerWidth > 767
+        }
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import './main.css'
+import './registerServiceWorker'
+
+createApp(App).mount('#app')
