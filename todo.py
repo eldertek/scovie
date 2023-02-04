33e8c150da46e940d753f540dd466352d0be23d6
André Théo LAURET <andrelauret@icloud.com>
2023-02-04 14:04:15 +0400

    remove debugging

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
 # TODO add internationalization
 # FIXME Fix fonts size
 # FIXME Custom admin page : custom planning view (with rooms, teachers, times), custom configuration view (with presets)
-# TODO Create a first-install script, that will add role (teacher, pce, admin) and create the first user
+# TODO Create a first-install script, that will add role (teacher, pce, admin) and create the first user 
