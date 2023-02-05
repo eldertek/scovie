f6c724d7c2a6a2f1187a129002d7f776b060c2fb
André Théo LAURET <andrelauret@icloud.com>
2023-02-05 22:59:59 +0400

    add static root

--- scovie/settings.py
+++ scovie/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
