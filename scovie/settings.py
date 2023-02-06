02d2e6e75652225f5bb2ae576528ace34cadd15a
André Théo LAURET <andrelauret@icloud.com>
2023-02-06 11:32:15 +0400

    modify screen static

--- scovie/settings.py
+++ scovie/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "screen/static"]
 STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
