4bd3f7daf07b104d3730861b46a3dd60180bbee4
André Théo LAURET <andrelauret@icloud.com>
2023-02-06 12:35:12 +0400

    delete static root

--- scovie/settings.py
+++ scovie/settings.py
@@ -133,8 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "screen/static"]
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
